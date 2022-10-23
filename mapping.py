c85dd3b568bff4b306ab229a83293b0050eb2d77
Clement Nardari <clement.nardari@gmail.com>
2022-10-23 18:09:17 -0400

    Room mapping working. Speed calib to be improved

--- mapping.py
+++ mapping.py
@@ -22,16 +22,16 @@ a=0
 yaw=0
 
 kp.init()
-#me = tello.Tello()
-#me.connect()
-#print(me.get_battery())
+me = tello.Tello()
+me.connect()
+print(me.get_battery())
 
 points=[(0,0), (0,0)]
 
 def getKeyboardInput():
     lr, fb, ud, yv = 0,0,0,0
     speed=15
-    aSpeed=50
+    aspeed=50
     global x,y,yaw,a
     d=0
     if kp.getKey("LEFT"): 
@@ -54,14 +54,16 @@ def getKeyboardInput():
         d=-dInterval
         a=-90
 
-    if kp.getKey("w"): ud=speed
-    elif kp.getKey("s"): ud=-speed
+    if kp.getKey("w"): 
+        ud=speed
+    elif kp.getKey("s"): 
+        ud=-speed
 
     if kp.getKey("a"): 
-        yv=aSpeed
+        yv=-aspeed
         yaw-=aInterval
     elif kp.getKey("d"): 
-        yv=-aSpeed
+        yv=aspeed
         yaw+=aInterval
 
     if kp.getKey("q"): me.land(); time.sleep(3)
@@ -82,13 +84,14 @@ def getKeyboardInput():
 def drawPoints(img,points):
     for point in points:
         cv2.circle(img,point,5,(0,0,255),cv2.FILLED)
+    cv2.circle(img,points[-1],8,(0,255,0),cv2.FILLED)
     cv2.putText(img,f'({(points[-1][0]-500)/100},{(points[-1][1]-500)/100})m',(points[-1][0]+10,points[-1][1]+30),cv2.FONT_HERSHEY_PLAIN,1,(255,0,255),1)
 
 
 
 while True:
     vals=getKeyboardInput()
-    #me.send_rc_control(vals[0],vals[1],vals[2],vals[3])
+    me.send_rc_control(vals[0],vals[1],vals[2],vals[3])
     img=np.zeros((1000,1000,3), np.uint8)
     if (points[-1][0] != vals[4] or points[-1][1] != vals[5]):
         points.append((vals[4],vals[5]))
